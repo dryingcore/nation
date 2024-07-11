165d026ed9a3b7594e7226631d17d9f9b647b37f
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-11 23:54:40 +0000

    Jul 11, 2024, 8:54 PM

---
+++ src/components/NoteCard.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+
+export default function NoteCard() {
+    return (
+        <Box sx={{ backgroundColor: 'orange', width: "100%", height: "100vh", padding: "1rem" }}>
+            <h1>a</h1>
+        </Box>
+    );
+}
