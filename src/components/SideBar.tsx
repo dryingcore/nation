b02ad57cf9581aff48a90c5889c053b8982f752f
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-11 23:09:45 +0000

    Jul 11, 2024, 8:09 PM

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,7 +1,10 @@
+import { Box } from "@mui/material";
+
 export default function SideBar() {
     return (
-        <div>
+        <Box sx={{ backgroundColor: 'red', width: "15%", height: "100vh" }}>
+            
             <h1>SideBar</h1>
-        </div>
+        </Box>
     )
 }
