2922769c4e840bd481a350c25fcb3f9fb1b880a2
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-11 23:13:54 +0000

    Jul 11, 2024, 8:13 PM

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,10 +1,25 @@
-import { Box } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 
 export default function SideBar() {
     return (
         <Box sx={{ backgroundColor: 'red', width: "15%", height: "100vh" }}>
-            
-            <h1>SideBar</h1>
+            <Grid container sx={{ backgroundColor: "blue" }}>
+                <Grid item xs={12} sx={{ backgroundColor: "green" }}>
+                    <Typography variant="body1">Search</Typography>
+                </Grid>
+
+                <Grid item xs={12} sx={{ backgroundColor: "yellow" }}>
+                    <Typography variant="body1">Home</Typography>
+                </Grid>
+
+                <Grid item xs={12} sx={{ backgroundColor: "purple" }}>
+                    <Typography variant="body1">Inbox</Typography>
+                </Grid>
+
+                <Grid item xs={12} sx={{ backgroundColor: "orange" }}>
+                    <Typography variant="body1">Settings & Members</Typography>
+                </Grid>
+            </Grid>
         </Box>
     )
 }
