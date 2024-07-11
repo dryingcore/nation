74591dd6d4b3afa366d3e5cbc8093c1252c82c10
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-11 23:42:39 +0000

    Jul 11, 2024, 8:42 PM

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,27 +1,24 @@
-import { Home, Inbox, Search, Settings } from "@mui/icons-material";
+import { Search } from "@mui/icons-material";
 import { Box, Grid, Typography } from "@mui/material";
 
-export default function SideBar() {
+function SideBar() {
     return (
-        <Box sx={{ backgroundColor: 'red', width: "15%", height: "100vh", padding: "1rem" }}>
-            <Grid container direction="column" spacing={2} sx={{ height: "100%", backgroundColor: "blue" }}>
+        <Box sx={{ backgroundColor: 'red', width: "100%", height: "100vh", padding: "1rem" }}>
+            <Grid container direction="column" spacing={2}>
                 <Grid item sx={{ display: "flex", alignItems: "center", backgroundColor: "green", padding: "0.5rem" }}>
                     <Search sx={{ fontSize: "1.4rem", marginRight: "0.5rem" }} />
                     <Typography variant="body1">Search</Typography>
                 </Grid>
 
                 <Grid item sx={{ display: "flex", alignItems: "center", backgroundColor: "yellow", padding: "0.5rem" }}>
-                    <Home sx={{ fontSize: "1.4rem", marginRight: "0.5rem" }} />
                     <Typography variant="body1">Home</Typography>
                 </Grid>
 
                 <Grid item sx={{ display: "flex", alignItems: "center", backgroundColor: "purple", padding: "0.5rem" }}>
-                    <Inbox sx={{ fontSize: "1.4rem", marginRight: "0.5rem" }} />
                     <Typography variant="body1">Inbox</Typography>
                 </Grid>
 
                 <Grid item sx={{ display: "flex", alignItems: "center", backgroundColor: "orange", padding: "0.5rem" }}>
-                    <Settings sx={{ fontSize: "1.4rem", marginRight: "0.5rem" }} />
                     <Typography variant="body1">Settings & Members</Typography>
                 </Grid>
             </Grid>
