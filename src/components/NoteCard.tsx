a4a5b8ac423205a01eda3aaed5614fa1e8fcd194
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-12 23:14:32 +0000

    Jul 12, 2024, 8:14 PM

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,3 +1,4 @@
+import { Edit } from "@mui/icons-material";
 import { Box, Card, CardContent, Typography } from "@mui/material";
 
 interface NoteCardProps {
@@ -8,8 +9,13 @@ const NoteCard: React.FC<NoteCardProps> = (props) => {
     return (
         <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: 'blue', width: "100%", height: "100%" }}>
             <Card sx={{ width: "100%", height: "100%", display: "flex", flexDirection: "column" }}>
-                <CardContent sx={{ flexGrow: 1,  }}>
+                <CardContent sx={{ flexGrow: 1, backgroundColor: 'lightblue', display: "flex" }}>
                     <Typography variant="body1">{props.title}</Typography>
+                    <Edit sx={{ fontSize: "1.4rem", marginRight: "0.5rem" }} />
+                </CardContent>
+
+                <CardContent>
+                    <Typography variant="body1">This is a note</Typography>
                 </CardContent>
             </Card>
         </Box>
