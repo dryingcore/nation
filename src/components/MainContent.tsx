8c305587ac9fc22b6b11b7ae9fb14d6ef7f362da
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-12 00:37:45 +0000

    Jul 11, 2024, 9:37 PM

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -4,7 +4,7 @@ import NoteCard from "./NoteCard";
 export default function MainContent() {
     return (
         <Box sx={{ backgroundColor: 'orange', width: "100%", height: "100vh", padding: "1rem", gap: "1rem" }}>
-            <NoteCard title="First nation note" />
+            <NoteCard title="" />
             <NoteCard title="First nation note" />
             <NoteCard title="First nation note" />
         </Box>
