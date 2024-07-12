a49894eece5549ac2c1229469be452ce558b3199
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-12 22:55:24 +0000

    Jul 12, 2024, 7:55 PM

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -6,7 +6,7 @@ interface NoteCardProps {
 
 const NoteCard: React.FC<NoteCardProps> = (props) => {
     return (
-        <Box sx={{ display: "flex", alignItems: "center", backgroundColor: 'blue', width: "30%", height: "30%", padding: "1rem" }}>
+        <Box sx={{ display: "flex", alignItems: "center", backgroundColor: 'blue', width: "30%", height: "30%", padding: "0.5rem" }}>
             <Card sx={{ width: "100%", height: "100%" }}>
                 <CardContent>
                     <Typography variant="body1">{props.title}</Typography>
