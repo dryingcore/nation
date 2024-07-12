f8a6d46b21edc182a5c1401ed3bfd1649039def3
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-12 22:56:11 +0000

    Jul 12, 2024, 7:56 PM

---
+++ src/components/MainContent.tsx
@@ -0,0 +1,19 @@
+import { Box, Grid } from "@mui/material";
+import NoteCard from "./NoteCard";
+
+export default function MainContent() {
+    // Array of titles for NoteCards
+    const titles = ["Hello, world!", "Hello, Kotlin!", "John Doe", "Jane Doe", "Lorem Ipsum", "Dolor Sit"];
+
+    return (
+        <Box sx={{ display: "flex", backgroundColor: 'orange', width: "100%", height: "100vh", padding: "1rem" }}>
+            <Grid container spacing={2} sx={{ width: "100%", height: "100%", overflow: "auto" }}>
+                {titles.map((title, index) => (
+                    <Grid item key={index} xs={12} sm={6} md={4}>
+                        <NoteCard title={title} />
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
+    );
+}
--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -6,9 +6,9 @@ interface NoteCardProps {
 
 const NoteCard: React.FC<NoteCardProps> = (props) => {
     return (
-        <Box sx={{ display: "flex", alignItems: "center", backgroundColor: 'blue', width: "30%", height: "30%", padding: "0.5rem" }}>
-            <Card sx={{ width: "100%", height: "100%" }}>
-                <CardContent>
+        <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: 'blue', width: "100%", height: "100%" }}>
+            <Card sx={{ width: "100%", height: "100%", display: "flex", flexDirection: "column" }}>
+                <CardContent sx={{ flexGrow: 1 }}>
                     <Typography variant="body1">{props.title}</Typography>
                 </CardContent>
             </Card>
