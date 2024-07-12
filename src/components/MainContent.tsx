ea26fa878e3b4b9660c59e4df51176147d42adc7
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-12 23:03:31 +0000

    Jul 12, 2024, 8:03 PM

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -3,33 +3,23 @@ import NoteCard from "./NoteCard";
 
 export default function MainContent() {
     // Array of titles for NoteCards
-    const notes = [{
-        title: "Note 1",
-    }, {
-        title: "Note 2",
-    }, {
-        title: "Note 3",
-    }, {
-        title: "Note 4",
-    }, {
-        title: "Note 5",
-    }, {
-        title: "Note 6",
-    }, {
-        title: "Note 7",
-    }, {
-        title: "Note 8",
-    }, {
-        title: "Note 9",
-    }, {
-        title: "Note 10",
-    }, {
-        title: "Note 11",
-    }]
+    const notes = [
+        { title: "Note 1" },
+        { title: "Note 2" },
+        { title: "Note 3" },
+        { title: "Note 4" },
+        { title: "Note 5" },
+        { title: "Note 6" },
+        { title: "Note 7" },
+        { title: "Note 8" },
+        { title: "Note 9" },
+        { title: "Note 10" },
+        { title: "Note 11" }
+    ];
 
     return (
         <Box sx={{ display: "flex", backgroundColor: 'orange', width: "100%", height: "100vh", padding: "1rem" }}>
-            <Grid container spacing={2} sx={{ width: "100%", height: "100%", overflow: "hidden", backgroundColor: 'darkgreen' }}>
+            <Grid container spacing={2} sx={{ width: "100%", height: "100%" }}>
                 {notes.map(({ title }, index) => (
                     <Grid item key={index} xs={12} sm={6} md={4}>
                         <NoteCard title={title} />
