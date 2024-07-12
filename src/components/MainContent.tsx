d3defad9ffa27a7df9cb30bf6005aadb39ef4398
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-12 00:36:58 +0000

    Jul 11, 2024, 9:36 PM

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,20 +1,12 @@
-import { Box, Grid } from "@mui/material";
+import { Box } from "@mui/material";
 import NoteCard from "./NoteCard";
 
 export default function MainContent() {
     return (
         <Box sx={{ backgroundColor: 'orange', width: "100%", height: "100vh", padding: "1rem", gap: "1rem" }}>
-            <Grid container spacing={2}>
-                <Grid item xs={12} sm={4}>
-                    <NoteCard title="First notion note" />
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <NoteCard title="Second notion note" />
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <NoteCard title="Third notion note" />
-                </Grid>
-            </Grid>
+            <NoteCard title="First nation note" />
+            <NoteCard title="First nation note" />
+            <NoteCard title="First nation note" />
         </Box>
     );
 }
