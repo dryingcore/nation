2fa7e677fc13ff55c4d51b82b36141c97fc56755
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-12 00:05:34 +0000

    Jul 11, 2024, 9:05 PM

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -5,6 +5,8 @@ export default function MainContent() {
     return (
         <Box sx={{ backgroundColor: 'orange', width: "100%", height: "100vh", padding: "1rem" }}>
             <NoteCard title="First nation note" />
+            <NoteCard title="First nation note" />
+            <NoteCard title="First nation note" />
         </Box>
     );
 }
