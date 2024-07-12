65d46dad65dbe05742ce7f0d96b77bbeb3dcfd12
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-12 22:58:31 +0000

    Jul 12, 2024, 7:58 PM

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -3,11 +3,11 @@ import NoteCard from "./NoteCard";
 
 export default function MainContent() {
     // Array of titles for NoteCards
-    const titles = ["Hello, world!", "Hello, Kotlin!", "John Doe", "Jane Doe", "Lorem Ipsum", "Dolor Sit"];
+    const titles = ["a"];
 
     return (
         <Box sx={{ display: "flex", backgroundColor: 'orange', width: "100%", height: "100vh", padding: "1rem" }}>
-            <Grid container spacing={2} sx={{ width: "100%", height: "100%", overflow: "auto" }}>
+            <Grid container spacing={2} sx={{ width: "100%", height: "100%", overflow: "hidden", backgroundColor: 'darkgreen' }}>
                 {titles.map((title, index) => (
                     <Grid item key={index} xs={12} sm={6} md={4}>
                         <NoteCard title={title} />
