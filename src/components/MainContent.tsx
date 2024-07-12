ba8bbdefc309b838a0d2ae350f9457ce1ecdab6f
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-12 23:04:44 +0000

    Jul 12, 2024, 8:04 PM

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -11,15 +11,12 @@ export default function MainContent() {
         { title: "Note 5" },
         { title: "Note 6" },
         { title: "Note 7" },
-        { title: "Note 8" },
-        { title: "Note 9" },
-        { title: "Note 10" },
-        { title: "Note 11" }
+        { title: "Note 8" }
     ];
 
     return (
         <Box sx={{ display: "flex", backgroundColor: 'orange', width: "100%", height: "100vh", padding: "1rem" }}>
-            <Grid container spacing={2} sx={{ width: "100%", height: "100%" }}>
+            <Grid container spacing={2} sx={{ width: "100%", height: "100%", backgroundColor: 'darkred' }}>
                 {notes.map(({ title }, index) => (
                     <Grid item key={index} xs={12} sm={6} md={4}>
                         <NoteCard title={title} />
