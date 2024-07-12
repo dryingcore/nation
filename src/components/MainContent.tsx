f9f80936dce3f57584a1f24db0c65a9814470e68
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-12 00:10:43 +0000

    Jul 11, 2024, 9:10 PM

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -3,12 +3,12 @@ import NoteCard from "./NoteCard";
 
 export default function MainContent() {
     return (
-        <Box sx={{ backgroundColor: 'orange', width: "100%", height: "100vh", padding: "1rem", gap: "1rem" }}>
+        <Box sx={{ backgroundColor: 'orange', width: "100%", height: "100vh", padding: "1rem", display: "flex", flexDirection: "column", alignItems: "center", gap: "1rem" }}>
 
-            <Box sx={{ display: "flex", flexDirection: "column", backgroundColor: 'purple', width: "100%", height: "30%", padding: "1rem", gap: "1rem" }}>
-                <NoteCard title="First nation note" />
-                <NoteCard title="First nation note" />
-                <NoteCard title="First nation note" />
+            <Box sx={{ backgroundColor: 'purple', width: "100%", padding: "1rem", display: "flex", flexDirection: "column", gap: "1rem" }}>
+                <NoteCard title="First notion note" />
+                <NoteCard title="Second notion note" />
+                <NoteCard title="Third notion note" />
             </Box>
         </Box>
     );
